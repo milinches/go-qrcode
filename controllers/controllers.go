2c1605cea4691ea9bfb9e14a4af2661c42e9a61e
milinches <ibrogenius5165@gmail.com>
2021-10-23 21:49:14 +0100

    Added views

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,9 +1,10 @@
 package controllers
 
 import (
+	"html/template"
 	"image/png"
+	"log"
 	"net/http"
-	"text/template"
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/qr"
@@ -14,14 +15,20 @@ type Page struct {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	p := Page{Title: "QR CODE GENERATOR"}
-	t, _ := template.ParseFiles("qr.html")
-	t.Execute(w, p)
+	t, err := template.ParseFiles("views/qr.html")
+	if err != nil {
+		log.Println("HTML file not found.")
+	}
+	if err := t.Execute(w, p); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func QRGenerator(w http.ResponseWriter, r *http.Request) {
 	data := r.FormValue("dataString")
 	qrCode, _ := qr.Encode(data, qr.L, qr.Auto)
-	qrCode, _ = barcode.Scale(qrCode, 512, 512)
+	qrCode, _ = barcode.Scale(qrCode, 300, 300)
 	png.Encode(w, qrCode)
 }
