e1d3c81b5f97c33fdd47ca3bdeacfdf9a7a97d70
milinches <ibrogenius5165@gmail.com>
2021-10-24 14:39:46 +0100

    static file working

--- routes/routes.go
+++ routes/routes.go
@@ -7,12 +7,12 @@ import (
 	"github.com/milinches/go-qrcode/controllers"
 )
 
-var fileServer = http.FileServer(http.Dir("./static"))
+var fileServer = http.FileServer(http.Dir("."))
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", controllers.Home).Methods("GET")
 	router.HandleFunc("/generate", controllers.QRGenerator).Methods("POST")
-	router.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	router.PathPrefix("/static/").Handler(fileServer)
 	return router
 }
