62c72cb626e500b60602715fb21358ab5c9bbef7
milinches <ibrogenius5165@gmail.com>
2021-10-24 13:34:48 +0100

    added static path

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,18 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/milinches/go-qrcode/controllers"
 )
+
+var fileServer = http.FileServer(http.Dir("./static"))
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", controllers.Home).Methods("GET")
 	router.HandleFunc("/generate", controllers.QRGenerator).Methods("POST")
+	router.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	return router
 }
